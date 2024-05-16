0306849d264f94680d1c372473c2d1d8a9b8869f
EjoGrejo <elio.greggio@gmail.com>
2024-05-16 16:29:22 +0200

    aggiunto progetto maven

---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////
+// Gabriele Di Pietro 2010000
+// Elio Greggio 2012831
+////////////////////////////////////////////////////////////////////
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
